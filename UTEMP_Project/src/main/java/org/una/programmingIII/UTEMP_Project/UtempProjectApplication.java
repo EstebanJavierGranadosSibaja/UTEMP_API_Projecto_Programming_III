d4299ec4e22bc304b7c991dfa2b780e6ebdedf28
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-06 16:52:43 -0600

    feat (application): Add first version of UtempProjectApplication

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/UtempProjectApplication.java
@@ -0,0 +1,13 @@
+package org.una.programmingIII.UTEMP_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtempProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtempProjectApplication.class, args);
+	}
+
+}
