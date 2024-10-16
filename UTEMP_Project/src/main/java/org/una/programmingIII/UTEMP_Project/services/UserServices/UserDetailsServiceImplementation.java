92dba19a47deb94038086367dc409f9d23b47da9
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-15 20:23:44 -0600

    feat (services): Add new UserDetailsService
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/services/UserServices/UserDetailsServiceImplementation.java
@@ -0,0 +1,31 @@
+package org.una.programmingIII.UTEMP_Project.services.UserServices;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.una.programmingIII.UTEMP_Project.models.User;
+import org.una.programmingIII.UTEMP_Project.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getName())
+                .password(user.getPassword())
+                .authorities(user.getAuthorities())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
