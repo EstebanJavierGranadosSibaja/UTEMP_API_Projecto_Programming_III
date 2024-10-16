b66f6252ab735d5cfa7ab7ab4178315e9c1387ae
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-15 20:28:30 -0600

    update (project-application): Add scanBasePackages in the UtempProjectApplication
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/UtempProjectApplication.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/UtempProjectApplication.java
@@ -3,7 +3,7 @@ package org.una.programmingIII.UTEMP_Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.una.programmingIII.UTEMP_Project")
 public class UtempProjectApplication {
 
 	public static void main(String[] args) {
