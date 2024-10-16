25d7a40491ab32dabdd98172ab054eb8e696c601
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-15 20:17:27 -0600

    update (repositories): Add new methods in UserRepository and EnrollmentRepository
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/EnrollmentRepository.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/EnrollmentRepository.java
@@ -1,7 +1,12 @@
 package org.una.programmingIII.UTEMP_Project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.una.programmingIII.UTEMP_Project.models.Course;
 import org.una.programmingIII.UTEMP_Project.models.Enrollment;
+import org.una.programmingIII.UTEMP_Project.models.User;
+
+import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    Optional<Enrollment> findByStudentAndCourse(User user, Course course);
 }
