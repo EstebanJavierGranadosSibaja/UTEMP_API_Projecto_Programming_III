f15162168fa1995ff8eb9c3b4df562037e67c5d3
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-15 20:04:30 -0600

    feat (exceptions): Add new exceptions for JWT
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/exceptions/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package org.una.programmingIII.UTEMP_Project.exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
