f2e351a2260eb8e4c94d09d89016b184b76aa5a8
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-15 20:17:27 -0600

    update (repositories): Add new methods in UserRepository and EnrollmentRepository
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/UserRepository.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package org.una.programmingIII.UTEMP_Project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.una.programmingIII.UTEMP_Project.models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
     Boolean existsByIdentificationNumber(String identificationNumber);
+    Optional<User> findByIdentificationNumber(String identificationNumber);
 }
