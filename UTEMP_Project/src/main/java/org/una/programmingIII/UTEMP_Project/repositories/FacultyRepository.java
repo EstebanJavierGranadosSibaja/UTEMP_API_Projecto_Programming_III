e642779c002562cb758451576770555f2bb577a4
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-17 13:09:53 -0600

    feat(repositories): Add new FacultyRepository
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/FacultyRepository.java
@@ -0,0 +1,7 @@
+package org.una.programmingIII.UTEMP_Project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.una.programmingIII.UTEMP_Project.models.Faculty;
+
+public interface FacultyRepository  extends JpaRepository<Faculty, Long> {
+}
