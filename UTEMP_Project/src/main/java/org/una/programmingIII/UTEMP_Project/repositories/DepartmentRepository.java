98d5f2131d002bc9df59520708c57b740592ff2e
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-17 13:09:17 -0600

    update (repositories): Add new DepartmentRepository
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package org.una.programmingIII.UTEMP_Project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.una.programmingIII.UTEMP_Project.models.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
