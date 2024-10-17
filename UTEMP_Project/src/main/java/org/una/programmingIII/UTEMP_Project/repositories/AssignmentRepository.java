fe1f7414d0f82f3c84bf040bad4e1d46fd64e661
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-17 13:06:36 -0600

    update (repositories): Add new findByCourse method
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/AssignmentRepository.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/AssignmentRepository.java
@@ -2,6 +2,11 @@ package org.una.programmingIII.UTEMP_Project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.una.programmingIII.UTEMP_Project.models.Assignment;
+import org.una.programmingIII.UTEMP_Project.models.Course;
+
+import java.util.List;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByCourse(Course course);
 }
+
