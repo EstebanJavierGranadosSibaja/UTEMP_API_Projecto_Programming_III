d45419443738939279e7221e75bba4423d85880f
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-17 13:08:07 -0600

    update (repositories): Add new findByTeacher and findByDepartment methods
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/CourseRepository.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/CourseRepository.java
@@ -2,6 +2,12 @@ package org.una.programmingIII.UTEMP_Project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.una.programmingIII.UTEMP_Project.models.Course;
+import org.una.programmingIII.UTEMP_Project.models.Department;
+import org.una.programmingIII.UTEMP_Project.models.User;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByTeacher(User user);
+    List<Course> findByDepartment(Department department);
 }
