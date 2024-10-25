f3d3f06ea01178697c10e4226ce49f9f156b70ef
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-24 23:12:53 -0600

    Update UtempProjectApplicationTests.java

--- UTEMP_Project/src/test/java/org/una/programmingIII/UTEMP_Project/UtempProjectApplicationTests.java
+++ UTEMP_Project/src/test/java/org/una/programmingIII/UTEMP_Project/UtempProjectApplicationTests.java
@@ -6,8 +6,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UtempProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Simplemente asegura que el contexto se carga sin errores
+    }
 
+    @Test
+    void testApplicationStarts() {
+        UtempProjectApplication.main(new String[]{});
+        // Puedes agregar más validaciones aquí si es necesario
+    }
+
+    // Añadir más tests para verificar componentes específicos si es necesario
 }
