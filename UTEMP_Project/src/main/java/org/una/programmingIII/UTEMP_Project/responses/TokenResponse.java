c969025ae7cf1bb4d8eaac37d9512336c44c3bcd
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-24 23:12:56 -0600

    Create TokenResponse.java

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/responses/TokenResponse.java
@@ -0,0 +1,20 @@
+package org.una.programmingIII.UTEMP_Project.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@Setter
+public class TokenResponse {
+    String token; //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+    String tokenType; //"Bearer",
+
+    @Value("${jwt.expiration.access}")
+    int expiresIn; //3600
+
+    TokenResponse(String token) {
+        this.token = token;
+        tokenType = "Bearer";
+    }
+}
