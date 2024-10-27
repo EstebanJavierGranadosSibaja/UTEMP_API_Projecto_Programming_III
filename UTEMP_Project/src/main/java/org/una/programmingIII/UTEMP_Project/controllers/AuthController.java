0a22a2cf8f9bd95d217588fdae36bdbc0458d830
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-26 22:02:56 -0600

    Create AuthController.java

---
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/controllers/AuthController.java
@@ -0,0 +1,53 @@
+package org.una.programmingIII.UTEMP_Project.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.una.programmingIII.UTEMP_Project.configurations.security.utils.JwtTokenProvider;
+import org.una.programmingIII.UTEMP_Project.controllers.request.AuthRequest;
+import org.una.programmingIII.UTEMP_Project.controllers.responses.ApiResponse;
+import org.una.programmingIII.UTEMP_Project.services.user.CustomUserDetails;
+import org.una.programmingIII.UTEMP_Project.services.user.CustomUserDetailsService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, CustomUserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<String>> createAuthenticationToken(@Valid @RequestBody AuthRequest authRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getIdentificationNumber(), authRequest.getPassword())
+            );
+
+            CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(authRequest.getIdentificationNumber());
+
+            if (userDetails == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "User not found"));
+            }
+
+            final String jwt = jwtTokenProvider.generateAccessToken(userDetails.getUser());
+            return ResponseEntity.ok(new ApiResponse<>("Bearer " + jwt));//TODO de bearer
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "Invalid credentials"));
+        }
+    }
+}
