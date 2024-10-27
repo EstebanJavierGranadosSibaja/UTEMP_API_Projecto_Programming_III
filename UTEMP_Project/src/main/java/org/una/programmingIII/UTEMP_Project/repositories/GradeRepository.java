a295470d5edcd3b440607192de78598b74576c75
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-10-26 18:48:50 -0600

    feat (service): Add AutoReviewService
    Co-Authored-By: JuanCaUNA <176434658+JuanCaUNA@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/GradeRepository.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/repositories/GradeRepository.java
@@ -5,7 +5,9 @@ import org.una.programmingIII.UTEMP_Project.models.Grade;
 import org.una.programmingIII.UTEMP_Project.models.Submission;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GradeRepository extends JpaRepository<Grade, Long> {
     List<Grade> findBySubmission (Submission submission);
+    Optional<Grade> findBySubmissionId(Long submissionId);
 }
