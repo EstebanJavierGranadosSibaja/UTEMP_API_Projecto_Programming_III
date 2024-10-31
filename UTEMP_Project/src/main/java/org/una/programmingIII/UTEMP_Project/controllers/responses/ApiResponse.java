15094b60f2a584ef678c410df93bef351f6ecfff
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-30 21:45:46 -0600

    Update ApiResponse.java

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/controllers/responses/ApiResponse.java
+++ UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/controllers/responses/ApiResponse.java
@@ -2,23 +2,32 @@ package org.una.programmingIII.UTEMP_Project.controllers.responses;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ApiResponse<T> {
     private T data;
     private int statusCode;
-    private String errorMessage;
+    private String message; // Cambiar a minúscula para seguir las convenciones de Java
+
+    public ApiResponse() {
+        data = null;
+        statusCode = 0;
+        message = "";
+
+    }
 
     public ApiResponse(T data) {
         this.data = data;
-        this.errorMessage = null; // Sin error
         this.statusCode = 200; // Código de estado por defecto
+        this.message = ""; // No hay mensaje por defecto
     }
 
     public ApiResponse(int statusCode, String errorMessage) {
         this.statusCode = statusCode;
-        this.errorMessage = errorMessage;
+        this.message = errorMessage;
         this.data = null; // Sin datos
     }
 
@@ -26,3 +35,4 @@ public class ApiResponse<T> {
         return new ApiResponse<>(statusCode, errorMessage);
     }
 }
+
