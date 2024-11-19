4827a60606c208c3aae70d8a8db916f8bc125009
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-11-18 23:39:28 -0600

    delete not use
    Co-Authored-By: Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
    Co-Authored-By: Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>

--- UTEMP_Project/src/main/java/org/una/programmingIII/UTEMP_Project/services/manualReview/ManualReviewService.java
+++
@@ -1,8 +0,0 @@
-package org.una.programmingIII.UTEMP_Project.services.manualReview;
-
-import org.una.programmingIII.UTEMP_Project.models.Grade;
-import java.util.Optional;
-
-public interface ManualReviewService {
-    Optional<Grade> ManualReviewSubmission(Long submissionId, double gradeValue, String comments);
-}
